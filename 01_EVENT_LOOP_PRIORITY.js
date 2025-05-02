46965d46d10f9d8df04051cc01dd80f20881fd40
sudipta08 <sudipta.bhattacharya762@gmail.com>
2025-05-02 15:29:16 +0530

    added event loop notes

--- 01_EVENT_LOOP_PRIORITY.js
+++ 01_EVENT_LOOP_PRIORITY.js
@@ -1,3 +1,20 @@
+// In every iteration, event loop comes across six different queues. Each queue holds one or more callback functions which need to be executed on call stack
+
+// nextTick
+// promises
+// setTimeout/setInterval
+// go back to micro task queues
+// i/o queue
+// go back to micro task queues
+// setImmediate
+// go back to micro task queues
+// close queue
+// go back to micro task queues
+// after every callback in settimeout, microtaskqueue is checked and then it moves on to other queues
+// when only one timer and i/o cb is present, depending on delay result will come
+// i/o polling checks if i/o operation is completed and then pushes the cb in i/o queue
+// i/o events are polled and added to i/o queue only after i/o is complete
+
 const baz = () => console.log('baz');
 const foo = () => console.log('foo');
 const zoo = () => console.log('zoo');
