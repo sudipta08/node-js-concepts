b3b481a06d2cfd9bdbf0f23aed7bafecacbedc25
sudipta08 <sudipta.bhattacharya762@gmail.com>
2025-05-06 09:54:39 +0530

    updated event loop priority

--- 01_EVENT_LOOP_PRIORITY.js
+++ 01_EVENT_LOOP_PRIORITY.js
@@ -1,19 +1,12 @@
 // In every iteration, event loop comes across six different queues. Each queue holds one or more callback functions which need to be executed on call stack
-
-// nextTick
-// promises
-// setTimeout/setInterval
-// go back to micro task queues
-// i/o queue
-// go back to micro task queues
-// setImmediate
-// go back to micro task queues
-// close queue
-// go back to micro task queues
 // after every callback in settimeout, microtaskqueue is checked and then it moves on to other queues
-// when only one timer and i/o cb is present, depending on delay result will come
+// when only one timer and i/o cb is present, depending on delay result will come - don't remember any more
+
 // i/o polling checks if i/o operation is completed and then pushes the cb in i/o queue
 // i/o events are polled and added to i/o queue only after i/o is complete
+setImmediate(() => console.log('set immediate'));
+const fs = require('fs');
+fs.readFile('./02_PROMISES.js', () => console.log('file read'));
 
 const baz = () => console.log('baz');
 const foo = () => console.log('foo');
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # node-js-concepts
+1. NPM - Node Package Manager is responsible for managing packages/modules for nodejs.
+2. Modules - It's a file which contains functions or variables which can be used in other parts of the application. In short, it's a reusable JS code.
+3. Callback function - It is a set of instructions which should be done after a task is completed. While the task is being done, the entire code execution will not be blocked.
